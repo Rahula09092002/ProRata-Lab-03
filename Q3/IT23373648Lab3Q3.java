6ed328d2c170f85e6c0815764b14087f03df2687
Rahula Basnayake <rahulabasnayake2002@gmail.com>
2026-09-17 22:23:11 +0530

    Add files via upload

---
+++ Q3/IT23373648Lab3Q3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class IT23373648Lab3Q3 {
+public static void main(String [] args){
+Scanner input = new Scanner(System.in);
+
+int amount;
+
+System.out.print("Enter anmount :");
+amount = input.nextInt();
+
+System.out.println("5000 Notes - " +(amount/5000));
+amount = amount % 5000; 
+
+System.out.println("1000 Notes - " +(amount/1000));
+amount = amount % 1000; 
+
+System.out.println("500 Notes - " +(amount/500));
+amount = amount % 500;
+
+System.out.println("200 Notes - " +(amount/200));
+amount = amount % 200;
+
+System.out.println("100 Notes - " +(amount/100));
+amount = amount % 100;
+
+System.out.println("50 Notes - " +(amount/50));
+amount = amount % 50;
+
+System.out.println("200 Notes - " +(amount/20));
+amount = amount % 20;
+
+System.out.println("10 Notes - " +(amount/10));
+amount = amount % 10;
+
+System.out.println("05 Notes - " +(amount/5));
+amount = amount % 5;
+
+System.out.println("02 Notes - " +(amount/2));
+amount = amount % 2; 
+
+System.out. println("01 Notes - " + amount);
+ }
+}
